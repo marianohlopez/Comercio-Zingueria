ceb40e01667bb4692de321746bd9dbe44f0ccb10
mlopez <ml.3012@gmail.com>
2022-07-06 15:00:13 -0300

    correccion

--- js/productos.js
+++ js/productos.js
@@ -24,17 +24,3 @@ for(item of productos){
 }
 
 
-
-/* const iva = x => x * 0.21;
-let costo = Number(prompt("Ingrese el costo del producto, o una letra para salir")); 
-
-while(Number(costo)){
-    let porcentaje = iva(costo);
-    alert(`el iva del producto es ${porcentaje}`);
-    let precioFinal = costo + porcentaje;
-    alert(`El precio final del producto es ${precioFinal}`);
-    costo = Number(prompt("Ingrese el costo del producto, o una letra para salir"));
-}
-alert("Gracias, vuelva pronto"); */
-
-
