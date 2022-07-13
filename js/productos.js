1672016033d3b236e69c943aaf4c8a50383707c7
mlopez <ml.3012@gmail.com>
2022-07-13 16:36:01 -0300

    interactuando

--- js/productos.js
+++ js/productos.js
@@ -21,13 +21,29 @@ while (entrada != "ESC"){
     entrada = prompt("Ingrese el nombre del producto que desea comprar o escriba ESC para salir:\nCAÑO REDONDO\nCAÑO RECTANGULAR\nCURVA ARTICULADA\nCURVA CORRUGADA\nSOMBRERO DOBLE ARO\nSOMBRERO VENTURI\nSOMBRERO TIPO H\nEOLICO DE ALUMINIO").toUpperCase()
 }
 
-for(item of carrito){
-    alert("Usted ingreso el producto " + item.nombre + " y su precio con iva incluido es " + iva(item.precio).toFixed(2))
+let estiloCarrito = document.getElementById("estiloCarrito");
+estiloCarrito.className = "estiloCarrito";
+
+let tituloSeleccion = document.getElementById("tituloCarrito");
+tituloSeleccion.innerHTML = `<h2>Usted selecciono los siguientes productos:</h2>`;
+
+let contenedorCarrito = document.getElementById("mensajeCarrito");
+
+for(const item of carrito){
+    let li = document.createElement("li")
+    li.innerHTML = `<h3>Producto: ${item.nombre}</h3>
+                    <b>Precio con iva: ${iva(item.precio).toFixed(2)}</b>
+                    `;
+    contenedorCarrito.append(li);
 }
 
 //SUMA DEL PRECIO DE LOS PRODUCTOS SELECCIONADOS
 
 let total = carrito.reduce((acumulador, elemento) => acumulador + iva(elemento.precio), 0);
 
-alert("El precio total es " + total.toFixed(2));
+let sumaTotal = document.getElementById("precioTotal");
+sumaTotal.innerHTML = `<h4>El precio total es ${total.toFixed(2)}</h4>`
+
+
+
 
