85cebd1d90cad354e43321e8896317c7c8c2d5f8
mlopez <ml.3012@gmail.com>
2022-07-26 16:02:17 -0300

    correccion

--- js/productos.js
+++ js/productos.js
@@ -1,7 +1,4 @@
 const productos = []
-/* const carrito = [];
-
-const iva = valor => valor * 1.21; */
 
 class Producto{
     constructor(id, nombre, precio, img, stock){
@@ -25,39 +22,3 @@ productos.push(new Producto(8, "EOLICO DE ALUMINIO", 2405, "../img/caño-redondo
 
 export {productos};
 
-
-//BUSQUEDA DE UNO O MAS PRODUCTOS E INGRESO EN CARRITO
-
-/* let entrada = prompt("Ingrese el nombre del producto que desea comprar o escriba ESC para salir:\nCAÑO REDONDO\nCAÑO RECTANGULAR\nCURVA ARTICULADA\nCURVA CORRUGADA\nSOMBRERO DOBLE ARO\nSOMBRERO VENTURI\nSOMBRERO TIPO H\nEOLICO DE ALUMINIO").toUpperCase() */
-/* 
-while (entrada != "ESC"){
-    let producto = productos.find(item => item.nombre === entrada);
-    carrito.push(producto);
-    entrada = prompt("Ingrese el nombre del producto que desea comprar o escriba ESC para salir:\nCAÑO REDONDO\nCAÑO RECTANGULAR\nCURVA ARTICULADA\nCURVA CORRUGADA\nSOMBRERO DOBLE ARO\nSOMBRERO VENTURI\nSOMBRERO TIPO H\nEOLICO DE ALUMINIO").toUpperCase()
-}
-
-let estiloCarrito = document.getElementById("estiloCarrito");
-estiloCarrito.className = "estiloCarrito";
-
-let tituloSeleccion = document.getElementById("tituloCarrito");
-tituloSeleccion.innerHTML = `<h2>Usted selecciono los siguientes productos:</h2>`;
-
-let contenedorCarrito = document.getElementById("mensajeCarrito");
-
-for(const item of carrito){
-    let li = document.createElement("li")
-    li.innerHTML = `<h3>Producto: ${item.nombre}</h3>
-                    <b>Precio con iva: ${iva(item.precio).toFixed(2)}</b>
-                    `;
-    contenedorCarrito.append(li);
-} */
-
-//SUMA DEL PRECIO DE LOS PRODUCTOS SELECCIONADOS
-
-/* let total = carrito.reduce((acumulador, elemento) => acumulador + iva(elemento.precio), 0);
-
-let sumaTotal = document.getElementById("precioTotal");
-sumaTotal.innerHTML = `<h4>El precio total es ${total.toFixed(2)}</h4>`
-
-
- */
